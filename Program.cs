13ea3dfd562ee2e5980ffb86df61f8f92c955473
Hycarus <mirkomarasco08@gmail.com>
2024-04-17 14:31:45 +0200

    snack 3 done

--- Program.cs
+++ Program.cs
@@ -5,32 +5,42 @@ class Program
     static void Main(string[] args)
     {
         // Snack 1
-        Console.WriteLine("Inserisci un numero");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Inserisci un altro numero");
-        int num2 = Convert.ToInt32(Console.ReadLine());
-        if (num1 > num2)
-        {
-            Console.WriteLine($"Il maggiore è: {num1}");
-        }
-        else
-        {
-            Console.WriteLine($"Il maggiore è: {num2}");
-        }
+        //Console.WriteLine("Inserisci un numero");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Inserisci un altro numero");
+        //int num2 = Convert.ToInt32(Console.ReadLine());
+        //if (num1 > num2)
+        //{
+        //    Console.WriteLine($"Il maggiore è: {num1}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"Il maggiore è: {num2}");
+        //}
 
-        // Snack 2
-        Console.WriteLine("Inserisci una parola");
-        string word1 = Console.ReadLine();
-        Console.WriteLine("Inserisci un altra parola");
-        string word2 = Console.ReadLine();
-        if(word1.Length > word2.Length)
+        //// Snack 2
+        //Console.WriteLine("Inserisci una parola");
+        //string? word1 = Console.ReadLine();
+        //Console.WriteLine("Inserisci un altra parola");
+        //string? word2 = Console.ReadLine();
+        //if(word1?.Length > word2?.Length)
+        //{
+        //    Console.WriteLine($"La parola più lunga è: {word1}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"La parola più lunga è: {word2}");
+        //}
+
+        // Snack 3
+        int sum = 0;
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine($"La parola più lunga è: {word1}");
+            Console.WriteLine("Inserisci un numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+            sum += num;
         }
-        else
-        {
-            Console.WriteLine($"La parola più lunga è: {word2}");
-        }
+        Console.WriteLine(sum);
     }
 }
 
